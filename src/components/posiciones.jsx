802e1f87e31aa47772b2923f85f461699ca25ee8
hlsalazar <hlsalazar@espe.edu.ec>
2024-06-20 12:38:12 -0500

    tabla muestra data v5

--- src/components/posiciones.jsx
+++ src/components/posiciones.jsx
@@ -1,40 +1,32 @@
 // src/components/Posiciones.jsx
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 
-const Posiciones = () => {
-  const [items, setItems] = useState([]);
-
-  useEffect(() => {
-    // Function to parse the query parameter
-    function getArrayFromQuery() {
-      const params = new URLSearchParams(window.location.search);
-      const data = params.get('gazeevents');
-      return data ? JSON.parse(decodeURIComponent(data)) : [];
-    }
-
-    const arrayFromQuery = getArrayFromQuery();
-    setItems(arrayFromQuery);
-  }, []);
+// Definir la interfaz de propiedades
+const Posiciones = ({ items }) => {
+  console.log("Received items in Posiciones:", items); // Añadir mensaje de consola para verificar los datos
 
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>Index</th>
-          <th>X Coordinate</th>
-          <th>Y Coordinate</th>
-        </tr>
-      </thead>
-      <tbody>
-        {items.map((item, index) => (
-          <tr key={index}>
-            <td>{index}</td>
-            <td>{item.docX}</td>
-            <td>{item.docY}</td>
+    <div>
+      <h2>Gaze Events</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>Index</th>
+            <th>X Coordinate</th>
+            <th>Y Coordinate</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {items.map((item, index) => (
+            <tr key={index}>
+              <td>{index}</td>
+              <td>{item.docX}</td>
+              <td>{item.docY}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 };
 
