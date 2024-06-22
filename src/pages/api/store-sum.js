7828a1b2d84d8f821d5789376e6b20035327f6a5
hlsalazar <hlsalazar@espe.edu.ec>
2024-06-22 18:09:02 -0500

    permisos CORS 3

--- src/pages/api/store-sum.js
+++ src/pages/api/store-sum.js
@@ -3,6 +3,16 @@ import { v4 as uuidv4 } from 'uuid';
 const dataStore = new Map();
 
 export async function post({ request }) {
+    if (request.method === 'OPTIONS') {
+        return new Response(null, {
+            headers: {
+                'Access-Control-Allow-Origin': 'https://api-seguim-ocular.vercel.app', // Tu origen permitido
+                'Access-Control-Allow-Methods': 'POST, GET, OPTIONS',
+                'Access-Control-Allow-Headers': 'Content-Type'
+            }
+        });
+    }
+
     const body = await request.json();
     const id = uuidv4();
     dataStore.set(id, body.sum);
@@ -19,14 +29,4 @@ export async function post({ request }) {
 
 export function getStore() {
     return dataStore;
-}
-
-export async function options() {
-    return new Response(null, {
-        headers: {
-            'Access-Control-Allow-Origin': 'https://api-seguim-ocular.vercel.app', // Tu origen permitido
-            'Access-Control-Allow-Methods': 'POST, GET, OPTIONS',
-            'Access-Control-Allow-Headers': 'Content-Type'
-        }
-    });
 }
