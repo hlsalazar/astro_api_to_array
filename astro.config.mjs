bf937ed7368a283897843824d8b5a5f59d92e12b
hlsalazar <hlsalazar@espe.edu.ec>
2024-06-23 12:11:20 -0500

    prueba api intermedia 2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
 import { defineConfig } from 'astro/config';
-
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server', // Agrega esta línea para habilitar soporte para rutas API
   integrations: [react()]
 });
