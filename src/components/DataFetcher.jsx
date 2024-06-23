d4eb4d1c7f7923d0fa8b04578b1e0856cf65f488
hlsalazar <hlsalazar@espe.edu.ec>
2024-06-23 18:01:36 -0500

    reinicioo 9

--- src/components/DataFetcher.jsx
+++ src/components/DataFetcher.jsx
@@ -1,31 +1,11 @@
+import React from 'react';
 
-import { useState, useEffect } from 'react';
-
-
-const DataFetcher = () => {
-        
-    const params = new URLSearchParams(Astro.url.search);
-    const id = params.get('id');
-    const [sum, setSum] = useState(null);
-    
-    useEffect(() => {
-      if (id) {
-        fetch(`/api/getstore`)
-          .then(response => response.json())
-          .then(data => {
-            const result = data.find(item => item[0] === id);
-            if (result) {
-              setSum(result[1]);
-            }
-          });
-      }
-    }, [id]);
-    
-    
+const SumResult = ({ sum }) => {
+  return (
     <div>
-      <h1>Sum Result: {sum !== null ? sum : 'Loading...'}</h1>
+      <h1>Sum Result: {sum !== null ? sum : 'No result yet'}</h1>
     </div>
-    
-}
+  );
+};
 
-export default DataFetcher;
+export default SumResult;
