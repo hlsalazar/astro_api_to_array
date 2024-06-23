0f998c959ad2d889121f070db2904a1d6b503a26
hlsalazar <hlsalazar@espe.edu.ec>
2024-06-23 16:10:34 -0500

    reinicioo 6

--- src/components/DataFetcher.jsx
+++ src/components/DataFetcher.jsx
@@ -1,30 +1,29 @@
-import React, { useEffect, useState } from 'react';
 
-const DataFetcher = ({ id }) => {
-  const [largeArray, setLargeArray] = useState([]);
+import { useState, useEffect } from 'react';
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const response = await fetch(`https://https://astro-api-to-array.vercel.app/api/get-data?id=${id}`);
-      const data = await response.json();
-      setLargeArray(data.largeArray);
-    };
 
-    if (id) {
-      fetchData();
-    }
-  }, [id]);
+const DataFetcher = () => {
+const params = Astro.url.searchParams;
+const id = params.get('id');
+const [sum, setSum] = useState(null);
 
-  return (
-    <div>
-      <h1>Datos recibidos</h1>
-      <ul>
-        {largeArray.map((item, index) => (
-          <li key={index}>{item}</li>
-        ))}
-      </ul>
-    </div>
-  );
-};
+useEffect(() => {
+  if (id) {
+    fetch(`/api/getstore`)
+      .then(response => response.json())
+      .then(data => {
+        const result = data.find(item => item[0] === id);
+        if (result) {
+          setSum(result[1]);
+        }
+      });
+  }
+}, [id]);
+
+
+<div>
+  <h1>Sum Result: {sum !== null ? sum : 'Loading...'}</h1>
+</div>
+}
 
 export default DataFetcher;
