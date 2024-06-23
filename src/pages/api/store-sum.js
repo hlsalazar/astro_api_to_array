02e40d73d29184e3f4b6302db20698323a81efb1
hlsalazar <hlsalazar@espe.edu.ec>
2024-06-23 11:51:48 -0500

    prueba api intermedia 1

--- src/pages/api/store-sum.js
+++ src/pages/api/store-sum.js
@@ -1,34 +1,5 @@
-import { v4 as uuidv4 } from 'uuid';
-
-const dataStore = new Map();
-
 export async function post({ request }) {
-    // Manejar la solicitud preflight OPTIONS
-    if (request.method === 'OPTIONS') {
-        return new Response(null, {
-            headers: {
-                'Access-Control-Allow-Origin': 'https://api-seguim-ocular.vercel.app', // Permitir tu origen específico
-                'Access-Control-Allow-Methods': 'POST, GET, OPTIONS',
-                'Access-Control-Allow-Headers': 'Content-Type'
-            }
-        });
-    }
-
-    // Manejar la solicitud POST
-    const body = await request.json();
-    const id = uuidv4();
-    dataStore.set(id, body.sum);
-
-    return new Response(JSON.stringify({ id: id }), {
-        headers: {
-            'Content-Type': 'application/json',
-            'Access-Control-Allow-Origin': 'https://api-seguim-ocular.vercel.app', // Permitir tu origen específico
-            'Access-Control-Allow-Methods': 'POST, GET, OPTIONS',
-            'Access-Control-Allow-Headers': 'Content-Type'
-        }
-    });
-}
-
-export function getStore() {
-    return dataStore;
-}
+    const { sum } = await request.json();
+    return new Response(JSON.stringify({ sum }), { status: 200 });
+  }
+  
