4f37b556187d4d59eb9f2b24f1197c5336597d5b
hlsalazar <hlsalazar@espe.edu.ec>
2024-06-23 12:18:13 -0500

    prueba api intermedia 5

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
 import { defineConfig } from 'astro/config';
-import react from "@astrojs/react";
-import vercel from '@astrojs/vercel';
 
+import react from "@astrojs/react";
+
+// https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  integrations: [react()],
-  adapter: vercel()
+  integrations: [react()]
 });
--- src/pages/api/store-sum.js
+++ src/pages/api/store-sum.js
@@ -1,5 +1,16 @@
 export async function post({ request }) {
     const { sum } = await request.json();
-    return new Response(JSON.stringify({ sum }), { status: 200 });
+    const headers = new Headers();
+    headers.set('Access-Control-Allow-Origin', '*'); // Permitir todos los orígenes. Puedes restringirlo a un origen específico si prefieres.
+    headers.set('Content-Type', 'application/json');
+    return new Response(JSON.stringify({ sum }), { status: 200, headers });
+  }
+  
+  export async function options() {
+    const headers = new Headers();
+    headers.set('Access-Control-Allow-Origin', '*'); // Permitir todos los orígenes. Puedes restringirlo a un origen específico si prefieres.
+    headers.set('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
+    headers.set('Access-Control-Allow-Headers', 'Content-Type');
+    return new Response(null, { status: 204, headers });
   }
   
